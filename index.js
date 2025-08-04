3646786d51596b76b997ce90ed9af2eecda55c29
BrunoJose18 <contatobrunojosebarboza@gmail.com>
2025-08-04 16:58:34 -0300

    Página quase concluída, faltam ajustes no espaçamento e nas setas

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*JS para alterar a cor da "fita" e links dos cards*/
 
-document.querySelectorAll(".card_caixa").forEach((card) => {
+document.querySelectorAll("[data-color]").forEach((card) => {
   const color = card.getAttribute("data-color");
   card.style.setProperty("--before-color", color);
 });
